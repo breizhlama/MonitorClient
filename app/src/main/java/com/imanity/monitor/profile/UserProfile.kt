d0c76cc6814e9bf8667603700292ad6c78c92273
samuelledrezen <samuel.le.drezen@gmail.com>
2017-08-08 21:57:43 -0400

    ajout de la classe user

---
+++ app/src/main/java/com/imanity/monitor/profile/UserProfile.kt
@@ -0,0 +1,6 @@
+package com.imanity.monitor.profile
+
+/**
+ * Created by samuelledrezen on 17-08-08.
+ */
+data class UserProfile(val name: String, val firstName: String, val username:String, val password:String, val mail:String)
